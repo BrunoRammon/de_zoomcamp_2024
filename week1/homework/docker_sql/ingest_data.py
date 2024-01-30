8f2466173bbbe4bfd7f7ddee4df5065721b805e1
BrunoRammon <BrunoRammon>
2024-01-30 20:09:20 -0300

    docker compose network and small changes

--- week1/homework/docker_sql/ingest_data.py
+++ week1/homework/docker_sql/ingest_data.py
@@ -16,15 +16,15 @@ def main(params):
     table_name = params.table_name
     engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-    file_name = 'green_tripdata_2019-09.csv'
+    file_name = 'green_tripdata_2019-09.csv.gz'
     os.system(f'wget {url} -O {file_name}')
 
     df_iter = pd.read_csv(f'{file_name}', iterator=True, chunksize=100000)
 
     df = next(df_iter)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
 
@@ -37,8 +37,8 @@ def main(params):
 
             df = next(df_iter)
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+            df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
 
             df.to_sql(name=table_name, con=engine, if_exists='append')
 
