1d2d44670097a85d80b801cb96d67f642cd1de06
BrunoRammon <BrunoRammon>
2024-02-08 18:42:36 -0300

    update homework week3 sql queries

--- week3/homework/queries.sql
+++ week3/homework/queries.sql
@@ -1,20 +1,25 @@
 CREATE OR REPLACE EXTERNAL TABLE `lexical-period-413309.green_taxi_data.external_green_tripdata`
 OPTIONS (
   format = 'parquet',
-  uris = ['https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2022-*.parquet']
-);
-
-CREATE OR REPLACE EXTERNAL TABLE `lexical-period-413309.green_taxi_data.external_green_tripdata`
-OPTIONS (
-  format = 'parquet',
   uris = ['gs://ny_taxi_data_dtc_bruno/green_tripdata_2022/green_tripdata_2022-*.parquet']
 );
 
-CREATE OR REPLACE TABLE taxi-rides-ny.nytaxi.green_tripdata AS
-SELECT * FROM taxi-rides-ny.nytaxi.external_green_tripdata;
+CREATE OR REPLACE TABLE lexical-period-413309.green_taxi_data.green_tripdata AS
+SELECT * FROM lexical-period-413309.green_taxi_data.external_green_tripdata;
 
-SELECT COUNT(*) FROM lexical-period-413309.green_taxi_data.green_tripdata;
+SELECT COUNT(*) 
+    FROM lexical-period-413309.green_taxi_data.green_tripdata;
 
-SELECT COUNT(DISTINCT(PULocationID)) FROM lexical-period-413309.green_taxi_data.external_green_tripdata;
+SELECT COUNT(DISTINCT(PULocationID)) 
+    FROM lexical-period-413309.green_taxi_data.external_green_tripdata;
 
-SELECT COUNT(DISTINCT(PULocationID)) FROM lexical-period-413309.green_taxi_data.green_tripdata;
+SELECT COUNT(DISTINCT(PULocationID)) 
+    FROM lexical-period-413309.green_taxi_data.green_tripdata;
+
+CREATE OR REPLACE TABLE lexical-period-413309.green_taxi_data.green_tripdata
+PARTITION BY DATE(lpep_pickup_datetime) AS
+SELECT * FROM lexical-period-413309.green_taxi_data.external_green_tripdata;
+
+SELECT DISTINCT(PULocationID) as trips
+FROM lexical-period-413309.green_taxi_data.green_tripdata_partitioned
+WHERE DATE(lpep_pickup_datetime) BETWEEN '2022-06-01' AND '2022-06-30';
